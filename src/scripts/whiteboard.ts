d39215070e3f4238fc6acb45d4efb4a15f6769e7
William Henderson <william-henderson@outlook.com>
2021-01-16 21:08:31 +0000

    Added undo functionality

--- src/scripts/whiteboard.ts
+++ src/scripts/whiteboard.ts
@@ -1,70 +1,98 @@
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
 var mouseDrawing = false;
-var stroke = [];
+var stroke: number[][] = [];
+
+var whiteboardHistory: string[] = [];
 
 ctx.lineWidth = 5;
 ctx.lineCap = "round";
 ctx.fillStyle = "#eee";
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+whiteboardHistory.push(canvas.toDataURL());
 
-function update(quality = 10) {
-  ctx.beginPath();
-  ctx.moveTo(stroke[0][0], stroke[0][1]);
+namespace Graphics {
+  export function update(quality = 10) {
+    ctx.beginPath();
+    ctx.moveTo(stroke[0][0], stroke[0][1]);
 
-  let smoothed = Smooth.Smooth(stroke, {
-    method: Smooth.METHOD_CUBIC,
-    clip: Smooth.CLIP_CLAMP
-  });
+    let smoothed = Smooth.Smooth(stroke, {
+      method: Smooth.METHOD_CUBIC,
+      clip: Smooth.CLIP_CLAMP
+    });
 
-  for (let i = 0; i < stroke.length - 1; i += 1 / quality) {
-    let calculatedPoint = smoothed(i);
-    ctx.lineTo(calculatedPoint[0], calculatedPoint[1]);
+    for (let i = 0; i < stroke.length - 1; i += 1 / quality) {
+      let calculatedPoint = smoothed(i);
+      ctx.lineTo(calculatedPoint[0], calculatedPoint[1]);
+    }
+
+    ctx.stroke();
   }
-
-  ctx.stroke();
 }
 
-function drawEventTouch(e) {
-  e.preventDefault();
-  stroke.push([e.touches[0].pageX, e.touches[0].pageY]);
-  if (stroke.length >= 5) stroke.splice(0, 1);
-  update();
+namespace Functionality {
+  export function undo() {
+    if (whiteboardHistory.length > 1) {
+      let imageToDraw = new Image();
+      whiteboardHistory.pop();
+      imageToDraw.src = whiteboardHistory[whiteboardHistory.length - 1];
+      imageToDraw.onload = () => { ctx.drawImage(imageToDraw, 0, 0) }
+    }
+  }
 }
 
-function startDrawTouch(e) {
-  e.preventDefault();
-  stroke = [[e.touches[0].pageX, e.touches[0].pageY]];
-}
-
-function drawEventMouse(e) {
-  if (e.buttons === 0) mouseDrawing = false;
-
-  if (mouseDrawing) {
-    stroke.push([e.offsetX, e.offsetY]);
+namespace Events {
+  export function drawEventTouch(e) {
+    e.preventDefault();
+    stroke.push([e.touches[0].pageX, e.touches[0].pageY]);
     if (stroke.length >= 5) stroke.splice(0, 1);
-    update();
+    Graphics.update();
   }
-}
 
-function startDrawMouse(e) {
-  mouseDrawing = true;
-  stroke = [[e.offsetX, e.offsetY]];
-}
+  export function startDrawTouch(e) {
+    e.preventDefault();
+    stroke = [[e.touches[0].pageX, e.touches[0].pageY]];
+  }
 
-function endDrawMouse(e) {
-  if (mouseDrawing) {
-    drawEventMouse(e);
-    mouseDrawing = false;
-    update();
+  export function endDrawTouch(e) {
+    e.preventDefault();
     stroke = [];
+    whiteboardHistory.push(canvas.toDataURL());
+    if (whiteboardHistory.length > 10) whiteboardHistory.shift();
+  }
+
+  export function drawEventMouse(e) {
+    if (e.buttons === 0) mouseDrawing = false;
+
+    if (mouseDrawing) {
+      stroke.push([e.offsetX, e.offsetY]);
+      if (stroke.length >= 5) stroke.splice(0, 1);
+      Graphics.update();
+    }
+  }
+
+  export function startDrawMouse(e) {
+    mouseDrawing = true;
+    stroke = [[e.offsetX, e.offsetY]];
+  }
+
+  export function endDrawMouse(e) {
+    if (mouseDrawing) {
+      drawEventMouse(e);
+      mouseDrawing = false;
+      Graphics.update();
+      stroke = [];
+      whiteboardHistory.push(canvas.toDataURL());
+      if (whiteboardHistory.length > 10) whiteboardHistory.shift();
+    }
   }
 }
 
-canvas.addEventListener("touchstart", startDrawTouch);
-canvas.addEventListener("touchmove", drawEventTouch);
-canvas.addEventListener("mousedown", startDrawMouse);
-canvas.addEventListener("mousemove", drawEventMouse);
-canvas.addEventListener("mouseup", endDrawMouse);
-canvas.addEventListener("mouseout", endDrawMouse)
+canvas.addEventListener("touchstart", Events.startDrawTouch);
+canvas.addEventListener("touchmove", Events.drawEventTouch);
+canvas.addEventListener("touchend", Events.endDrawTouch);
+canvas.addEventListener("mousedown", Events.startDrawMouse);
+canvas.addEventListener("mousemove", Events.drawEventMouse);
+canvas.addEventListener("mouseup", Events.endDrawMouse);
+canvas.addEventListener("mouseout", Events.endDrawMouse);
